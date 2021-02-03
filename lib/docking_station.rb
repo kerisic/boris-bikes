3382be1cd86f7ef6b0e4c48b9d4537a172c628d7
Max Clarke <maxclarke@ma368.lan>
2021-02-03 15:14:05 +0000

    step 11 done

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,6 +4,10 @@ class DockingStation
   def release_bike
     Bike.new
   end
+  def dock(bike)
+    @bike = bike
+  end
+  attr_reader :bike
 end
 
 class Bike
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,13 +1,19 @@
 require 'docking_station'
 
 describe 'DockingStationTest' do
+  docking_station = DockingStation.new
+  bike = docking_station.release_bike
   it 'responds to release_bike' do
-    docking_station = DockingStation.new
     expect(docking_station).to respond_to(:release_bike)
   end
   it 'get the bike and check if bike is working' do
-    docking_station = DockingStation.new
     bike = docking_station.release_bike
     expect(bike).to be_working
+  end
+  it 'docking the bike at the docking station' do
+    expect(docking_station).to respond_to(:dock)
+  end
+  it 'the bike is stored at the docking station' do
+    expect(docking_station.dock(bike)).to eq(bike)
   end
 end
